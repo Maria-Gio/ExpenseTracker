ce7d7f0abfffb47dd948c41401d267092f4f5f12
Maria Gio <alvarezgiomariaisabel@gmail.com>
2023-05-19 13:55:28 +0200

    predowngrade

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: []
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registroForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.registroForm = this.fb.group({});
+  }
+  ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+  crearUsuario(): void {
+    console.log(this.registroForm);
+
+    console.log(this.registroForm.valid);
+    console.log(this.registroForm.value);
+  }
 
 }
